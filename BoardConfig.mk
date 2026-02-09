3b2cc82d5e14fa23f3c5787faaa073ecc55e2620
nullptr03 <ayamgorengmanis87@gmail.com>
2026-02-09 14:37:21 +0000

    garnet: Compile HWUI for performance

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,6 +84,9 @@ $(foreach sku, CN GL JP, \
 
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/framework_manifest.xml
 
+# Optimize HWUI for performance
+HWUI_COMPILE_FOR_PERF := true
+
 # Kernel
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_RAMDISK_USE_LZ4 := true
