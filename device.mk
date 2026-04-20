68afef939a7abfe06c60b796d820acb7a4e81efd
chuspita <kosecki99@gmail.com>
2026-04-20 10:01:55 +0200

    garnet: enable Vulkan support (v1.3)
    - Enable Vulkan support by setting TARGET_USES_VULKAN and declaring Vulkan 1.3 + level-1 hardware features via permissions XML. Ensures proper feature exposure for apps and modern rendering support.
    
    Signed-off-by: chuspita <kosecki99@gmail.com>

--- device.mk
+++ device.mk
@@ -180,6 +180,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml
+
 # GPS
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
