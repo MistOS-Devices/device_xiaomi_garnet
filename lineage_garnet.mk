ce4e6df9fa66d3add05cd720fe8324e69bfdd91c
chuspita <kosecki99@gmail.com>
2026-04-22 19:30:23 +0200

    garnet: Comment out WITH_GMS variable

--- lineage_garnet.mk
+++ lineage_garnet.mk
@@ -18,7 +18,6 @@ $(call inherit-product, device/xiaomi/garnet/device.mk)
 # MistoS stuff
 MISTOS_MAINTAINER := franlop77
 TARGET_ENABLE_BLUR := true
-WITH_GMS := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_HAS_UDFPS := true
 EXTRA_UDFPS_ANIMATIONS := true
